7fb9a97b601656ee02f6cc9a69de5b1cdf05a36e
Abhi Gulati <abhigulati@gmail.com>
2016-05-30 12:17:33 -0500

    server also accepts image URL in POST body

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const _ = require('lodash')
-const app = express();
 const checkIfCatImage = require('./catternaut');
+
+const app = express();
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   const imageURL = _.get(req, 'query.image');
   if (!imageURL) {
     res.send('Provide the image URL as the "image" query parameter')
+  }
+
+  checkIfCatImage(imageURL, 10, function(isCatImage) {
+    const isCatLabel = isCatImage ? 'CAT' : 'NOT CAT';
+    res.send(`<img src=${imageURL}><h3>${isCatLabel}</h3>`);
+  })
+});
+
+app.post('/', function(req, res) {
+  const imageURL = _.get(req, 'body.image');
+  if (!imageURL) {
+    res.send('Provide the image URL as the "image" property in the POST body')
   }
 
   checkIfCatImage(imageURL, 10, function(isCatImage) {
