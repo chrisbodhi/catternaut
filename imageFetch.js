2862c26b7410706b8e5e3aca22df314f0f460321
Chris Boette <cboette@gmail.com>
2016-06-03 22:49:56 -0500

    Add entry point for module. Add README.
    WIP on conversion to module.
    
    Add supported file types to README.
    
    In process of moving from callbacks to promises.
    
    Some README updates.

--- imageFetch.js
+++ imageFetch.js
@@ -1,19 +1,20 @@
-var request = require('request')
-var fs = require('fs')
+const request = require('request')
+const fs = require('fs')
 
 function fetchAndSaveImage(imageURL, pathToSave = 'image.jpeg') {
   request(imageURL)
-    .pipe(fs.createWriteStream(pathToSave))
+    .pipe(fs.createWriteStream(pathToSave));
 }
-//'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ4wdmW2mGrTj4In2et-g780oVp50gDVzObkaSSGZP94PHlFLN'
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ4wdmW2mGrTj4In2et-g780oVp50gDVzObkaSSGZP94PHlFLN
+// ^^^ Image of a cat
 
 if (module === require.main) {
   if (process.argv.length !== 4) {
     console.log('Usage: node imageFetch <imageURL> <pathToSave>');
     process.exit(1);
   }
-  var imageURL = process.argv[2];
-  var pathToSave = process.argv[3];
+  const imageURL = process.argv[2];
+  const pathToSave = process.argv[3];
   fetchAndSaveImage(imageURL, pathToSave);
 }
 
